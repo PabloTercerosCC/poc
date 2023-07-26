ac71b500cc0c642e88b1486604ab0cd352d1d3d5
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2023-07-25 20:42:08 -0400

    dictionary impl

--- src/main/java/org/example/TranscriberDemo.java
+++ src/main/java/org/example/TranscriberDemo.java
@@ -1,7 +1,28 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+
+public class TranscriberDemo {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration configuration = new Configuration();
+
+        configuration.setAcousticModelPath("/home/fundacion/Documents/sem6/prog6/code/sphinx-poc/src/main/java/org/example/dictionary/cmusphinx-es-5.2/model_parameters/voxforge_es_sphinx.cd_ptm_4000");
+        configuration.setDictionaryPath("/home/fundacion/Documents/sem6/prog6/code/sphinx-poc/src/main/java/org/example/dictionary/es.dict");
+        configuration.setLanguageModelPath("/home/fundacion/Documents/sem6/prog6/code/sphinx-poc/src/main/java/org/example/dictionary/es-20k.lm");
+
+        LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
+
+        System.out.println("Escuchando...");
+
+        recognizer.startRecognition(true);
+        SpeechResult result;
+        while ((result = recognizer.getResult()) != null) {
+            System.out.format("Hipótesis: %s\n", result.getHypothesis());
+        }
+        recognizer.stopRecognition();
     }
 }
